440deb888b690faea15f0a310aa54a55b609e04d
Eric Peterson <peterson.eric.c@gmail.com>
2022-12-03 09:14:07 -0800

    add some comments

--- 2022/day_2.erl
+++ 2022/day_2.erl
@@ -1,15 +1,17 @@
 %%%% 2022/day_2.erl
-%%%%
-%%%% 
 
 -module(day_2).
 -export([start_1/1, start_2/1]).
+
+-type move() :: integer().     % 0 to 2, where 2 beats 1 beats 0 beats 2.
+-type outcome() :: integer().  % 0 lose, 1 draw, 2 win
 
 %%% Entry point for Part 1.
 -spec start_1([string()]) -> no_return().
 start_1([Filename]) ->
     {ok, Handle} = file:open(Filename, read),
-    Score = calculate_score(Handle),
+    Score = calculate_score_with_moves(Handle, 0),
+    file:close(Handle),
 
     io:format("~w~n", [Score]),
     halt(0).
@@ -18,47 +20,65 @@ start_1([Filename]) ->
 -spec start_2([string()]) -> no_return().
 start_2([Filename]) ->
     {ok, Handle} = file:open(Filename, read),
-    Score = calculate_score_with_strategy(Handle),
+    Score = calculate_score_with_strategy(Handle, 0),
+    file:close(Handle),
 
     io:format("~w~n", [Score]),
     halt(0).
 
--spec calculate_score(file:io_device()) -> integer().
-calculate_score(Handle) ->
-    calculate_score(Handle, 0).
-
--spec calculate_score(file:io_device(), integer()) -> integer().
-calculate_score(Handle, Total) ->
+%%% Calculate the score of a tournament, where the second column is understood
+%%% to record our move.
+-spec calculate_score_with_moves(file:io_device(), integer()) -> integer().
+calculate_score_with_moves(Handle, Total) ->
     case io:get_line(Handle, none) of
         eof -> Total;
         String ->
-            [Opponent, _, Move] = string:strip(String, both, $\n),
-            OpponentInt = (Opponent - $A) + 1,
-            MoveInt = (Move - $X) + 1,
+            [TheirMoveChar, _, MyMoveChar | _] = String,
+            TheirMove = move_from_char(TheirMoveChar),
+            MyMove = move_from_char(MyMoveChar - $X + $A),
 
-            MoveScore = MoveInt,
-            OutcomeScore = 3 * (((MoveInt - OpponentInt + 1) + 3) rem 3),
-            calculate_score(Handle, Total + OutcomeScore + MoveScore)
+            Outcome = outcome_from_moves(TheirMove, MyMove),
+            GameScore = score_game(MyMove, Outcome),
+            calculate_score_with_moves(Handle, Total + GameScore)
     end.
 
--spec calculate_score_with_strategy(file:io_device()) -> integer().
-calculate_score_with_strategy(Handle) ->
-    calculate_score_with_strategy(Handle, 0).
-
+%%% Calculate the score of a tournament, where the second column is understood
+%%% to record the game outcome.
 -spec calculate_score_with_strategy(file:io_device(), integer()) -> integer().
 calculate_score_with_strategy(Handle, Total) ->
     case io:get_line(Handle, none) of
         eof -> Total;
         String ->
-            [Opponent, _, Outcome] = string:strip(String, both, $\n),
-            OpponentInt = Opponent - $A,
-            OutcomeInt = Outcome - $X,
+            [TheirMoveChar, _, OutcomeChar | _] = String,
+            TheirMove = move_from_char(TheirMoveChar),
+            Outcome = outcome_from_char(OutcomeChar),
 
-            MoveInt = ((OpponentInt + (OutcomeInt - 1) + 3) rem 3),
-            MoveScore = MoveInt + 1,
-            OutcomeScore = OutcomeInt * 3,
-            calculate_score_with_strategy(
-                Handle,
-                Total + OutcomeScore + MoveScore
-            )
+            MyMove = move_from_outcome(TheirMove, Outcome),
+            GameScore = score_game(MyMove, Outcome),
+            calculate_score_with_strategy(Handle, Total + GameScore)
     end.
+
+%%% Calculate the outcome of a game.
+-spec outcome_from_moves(move(), move()) -> outcome().
+outcome_from_moves(TheirMove, MyMove) ->
+    ((MyMove - TheirMove + 1) + 3) rem 3.
+
+%%% Calculate what move will result in the indicated outcome.
+-spec move_from_outcome(move(), outcome()) -> move().
+move_from_outcome(TheirMove, Outcome) ->
+    (((TheirMove - 1) + (Outcome - 1) + 3) rem 3) + 1.
+
+%%% Deserialize a move.
+-spec move_from_char(integer()) -> move().
+move_from_char(Char) ->
+    Char - $A + 1.
+
+%%% Deserialize an outcome.
+-spec outcome_from_char(integer()) -> outcome().
+outcome_from_char(Char) ->
+    Char - $X.
+
+%%% Compute our score from a game.
+-spec score_game(move(), outcome()) -> integer().
+score_game(Move, Outcome) ->
+    Move + Outcome * 3.
--- 2022/day_3.erl
+++ 2022/day_3.erl
@@ -3,22 +3,27 @@
 -module(day_3).
 -export([start_1/1, start_2/1]).
 
+%%% Entry point for Part 1.
+-spec start_1([string()]) -> no_return().
 start_1([Filename]) ->
     {ok, Handle} = file:open(Filename, read),
-    Priorities = duplicates(Handle),
+    Priorities = duplicates(Handle, []),
     file:close(Handle),
     io:format("~w~n", [lists:sum(lists:map(fun priority/1, Priorities))]),
     halt(0).
 
+%%% Entry point for Part 2.
+-spec start_1([string()]) -> no_return().
 start_2([Filename]) ->
     {ok, Handle} = file:open(Filename, read),
-    Priorities = badges(Handle),
+    Priorities = badges(Handle, []),
     file:close(Handle),
     io:format("~w~n", [lists:sum(lists:map(fun priority/1, Priorities))]),
     halt(0).
 
-duplicates(Handle) -> duplicates(Handle, []).
-
+%%% For each line in Handle, find the unique character which is common to the
+%%% first and second halves of the line.
+-spec duplicates(file:io_device(), [integer()]) -> [integer()].
 duplicates(Handle, Acc) ->
     case io:get_line(Handle, none) of
         eof -> Acc;
@@ -32,8 +37,9 @@ duplicates(Handle, Acc) ->
             duplicates(Handle, [Duplicate | Acc])
     end.
 
-badges(Handle) -> badges(Handle, []).
-
+%%% For each triple of lines in Handle, find the unique character which is
+%%% common to all three lines.
+-spec badges(file:io_device(), [integer()]) -> [integer()].
 badges(Handle, Acc) ->
     case [io:get_line(Handle, none) || _ <- lists:seq(1, 3)] of
         [eof, eof, eof] -> Acc;
@@ -45,15 +51,21 @@ badges(Handle, Acc) ->
             badges(Handle, [Badge|Acc])
     end.
 
+%%% Divide a string into two halves.
+-spec split_halves(string()) -> {string(), string()}.
 split_halves(Line) ->
     Length = length(Line),
     FirstHalf = lists:sublist(Line, Length div 2),
     SecondHalf = lists:sublist(Line, Length div 2 + 1, Length div 2),
     {FirstHalf, SecondHalf}.
 
+%%% Remove any carriage returns from a line.
+-spec strip(string()) -> string().
 strip(Line) ->
     string:strip(Line, both, $\n).
 
+%%% Assigns a priority to a character in A-Za-z.
+-spec priority(integer()) -> integer().
 priority(Char) when Char >= $a andalso Char =< $z ->
     Char - $a + 1;
 priority(Char) when Char >= $A andalso Char =< $Z ->
